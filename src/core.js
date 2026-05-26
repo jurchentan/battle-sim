aff93735b4c210b2412f19ddd9e57c4b36b2b07c
Alex Tom <atom2338@gmail.com>
2026-05-26 19:25:15 -0400

    Move charge description below ability bar, add super name display, italicize charge text

--- src/core.js
+++ src/core.js
@@ -209,6 +209,8 @@ const els = {
   reelsRedName: document.getElementById("reelsRedName"),
   reelsBlueTraits: document.getElementById("reelsBlueTraits"),
   reelsRedTraits: document.getElementById("reelsRedTraits"),
+  reelsBlueSuperName: document.getElementById("reelsBlueSuperName"),
+  reelsRedSuperName: document.getElementById("reelsRedSuperName"),
   reelsBlueQuote: document.getElementById("reelsBlueQuote"),
   reelsRedQuote: document.getElementById("reelsRedQuote"),
   reelsBlueMiniTraits: document.getElementById("reelsBlueMiniTraits"),
